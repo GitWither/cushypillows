dde4a987f6838dc69b78f71bdc6f3d83ba5be1e2
SirJain <94301223+SirJain0@users.noreply.github.com>
2024-08-14 21:49:05 +0100

    Fix datagen for recipes

--- src/main/java/daniel/cushypillows/data/CushyPillowsDataGenerator.java
+++ src/main/java/daniel/cushypillows/data/CushyPillowsDataGenerator.java
@@ -1,29 +1,34 @@
 package daniel.cushypillows.data;
 
+import com.google.common.collect.Maps;
 import daniel.cushypillows.CushyPillows;
 import daniel.cushypillows.block.CushyPillowsBlocks;
-import daniel.cushypillows.block.entity.CushyPillowsBlockEntities;
-import daniel.cushypillows.item.CushyPillowsItems;
+import daniel.cushypillows.block.PillowBlock;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricModelProvider;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
+import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
-import net.minecraft.data.client.*;
+import net.minecraft.data.client.BlockStateModelGenerator;
+import net.minecraft.data.client.ItemModelGenerator;
+import net.minecraft.data.client.Model;
 import net.minecraft.data.server.recipe.RecipeJsonProvider;
 import net.minecraft.data.server.recipe.ShapedRecipeJsonBuilder;
+import net.minecraft.item.DyeItem;
+import net.minecraft.item.Item;
 import net.minecraft.item.Items;
 import net.minecraft.recipe.book.RecipeCategory;
-import net.minecraft.registry.Registries;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.registry.RegistryWrapper;
-import net.minecraft.registry.tag.BlockTags;
 import net.minecraft.registry.tag.TagKey;
+import net.minecraft.util.DyeColor;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.Util;
 
-import java.util.List;
+import java.util.EnumMap;
 import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 import java.util.function.Consumer;
@@ -113,16 +118,42 @@ public class CushyPillowsDataGenerator implements DataGeneratorEntrypoint {
 
         @Override
         public void generate(Consumer<RecipeJsonProvider> exporter) {
-            ShapedRecipeJsonBuilder.create(RecipeCategory.MISC, CushyPillowsItems.WHITE_PILLOW)
-                    .pattern("WFW")
-                    .pattern(" D ")
-                    .input('F', Items.FEATHER)
-                    .input('D', Items.WHITE_DYE)
-                    .input('W', Items.WHITE_WOOL)
-                    .criterion(hasItem(Items.FEATHER), conditionsFromItem(Items.FEATHER))
-                    .criterion(hasItem(Items.WHITE_DYE), conditionsFromItem(Items.WHITE_DYE))
-                    .criterion(hasItem(Items.WHITE_WOOL), conditionsFromItem(Items.WHITE_WOOL))
-                    .offerTo(exporter, new Identifier(getRecipeName(CushyPillowsItems.WHITE_PILLOW)));
+            EnumMap<DyeColor, Block> dyeToWool = Util.make(Maps.newEnumMap(DyeColor.class), (map) -> {
+                map.put(DyeColor.WHITE, Blocks.WHITE_WOOL);
+                map.put(DyeColor.ORANGE, Blocks.ORANGE_WOOL);
+                map.put(DyeColor.MAGENTA, Blocks.MAGENTA_WOOL);
+                map.put(DyeColor.LIGHT_BLUE, Blocks.LIGHT_BLUE_WOOL);
+                map.put(DyeColor.YELLOW, Blocks.YELLOW_WOOL);
+                map.put(DyeColor.LIME, Blocks.LIME_WOOL);
+                map.put(DyeColor.PINK, Blocks.PINK_WOOL);
+                map.put(DyeColor.GRAY, Blocks.GRAY_WOOL);
+                map.put(DyeColor.LIGHT_GRAY, Blocks.LIGHT_GRAY_WOOL);
+                map.put(DyeColor.CYAN, Blocks.CYAN_WOOL);
+                map.put(DyeColor.PURPLE, Blocks.PURPLE_WOOL);
+                map.put(DyeColor.BLUE, Blocks.BLUE_WOOL);
+                map.put(DyeColor.BROWN, Blocks.BROWN_WOOL);
+                map.put(DyeColor.GREEN, Blocks.GREEN_WOOL);
+                map.put(DyeColor.RED, Blocks.RED_WOOL);
+                map.put(DyeColor.BLACK, Blocks.BLACK_WOOL);
+            });
+
+            for (DyeColor color : DyeColor.values()) {
+                Block pillow = PillowBlock.getForColor(color);
+                Item dye = DyeItem.byColor(color);
+                Blocks.WHITE_WOOL.getDefaultMapColor();
+                Block wool = dyeToWool.get(color);
+
+                ShapedRecipeJsonBuilder.create(RecipeCategory.MISC, pillow)
+                        .pattern("WFW")
+                        .pattern(" D ")
+                        .input('F', Items.FEATHER)
+                        .input('D', dye)
+                        .input('W', wool.asItem())
+                        .criterion(hasItem(Items.FEATHER), conditionsFromItem(Items.FEATHER))
+                        .criterion(hasItem(dye), conditionsFromItem(dye))
+                        .criterion(hasItem(wool), conditionsFromItem(Items.WHITE_WOOL))
+                        .offerTo(exporter, new Identifier(getRecipeName(pillow)));
+            }
         }
     }
 }
