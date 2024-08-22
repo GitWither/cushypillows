5da615d4010aa62576c77a123f4ba8cbb0ffaaa9
SirJain <94301223+SirJain0@users.noreply.github.com>
2024-08-22 13:36:17 -0400

    Update break particle

--- src/main/java/daniel/cushypillows/data/CushyPillowsDataGenerator.java
+++ src/main/java/daniel/cushypillows/data/CushyPillowsDataGenerator.java
@@ -318,7 +318,7 @@ public class CushyPillowsDataGenerator implements DataGeneratorEntrypoint {
             Model templatePillow = new Model(Optional.of(new Identifier(CushyPillows.MOD_ID, "item/template_pillow")), Optional.empty());
             blockStateModelGenerator.registerBuiltin(
                     new Identifier(CushyPillows.MOD_ID, "block/pillow"),
-                    Blocks.OAK_PLANKS
+                    CushyPillowsBlocks.WHITE_PILLOW
             ).includeWithItem(templatePillow,
                     CushyPillowsBlocks.WHITE_PILLOW,
                     CushyPillowsBlocks.ORANGE_PILLOW,
